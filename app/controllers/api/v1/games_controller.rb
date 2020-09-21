6f9c2a180d200bbee1ed0a7c11e04f90e303f93d
tpope0928 <tpope0928@gmail.com>
2020-09-21 19:42:06 -0400

    games controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -3,4 +3,21 @@ class Api::V1::GamesController < ApplicationController
     games = Game.all
     render json: games
   end
+
+  def create
+    game = Game.create(game_params)
+    game.save!
+    render json: game
+  end
+
+  def destroy
+    game = Game.find_by(:id => params[:id]).destroy
+    render json: game
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :genre, :username, :player_id)
+  end
 end
