cfb151c378ed52e557810dc289ca31f9b09442ca
tpope0928 <tpope0928@gmail.com>
2020-09-21 19:37:02 -0400

    player controller

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::PlayersController < ApplicationController
+  def index
+    players = Player.all
+    render json: players
+  end
+
+  def create
+    if Player.find_by(:name => player_params[:name])
+      player = Player.find_by(:name => player_params[:name])
+      redirect_to "api/v1/players/#{player.id}"
+    else
+      player = Player.create(player_params)
+      player.save!
+      render json: player
+    end
+  end
+
+  def show
+    player = Player.find_by(:id => params[:id])
+    render json: player
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :city, :state)
+  end
 end
