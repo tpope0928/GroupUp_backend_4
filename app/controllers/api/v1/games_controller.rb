386e0ea058e6b918e19599925f41850b7e46413d
tpope0928 <tpope0928@gmail.com>
2020-09-27 18:11:16 -0400

    back to normal

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::GamesController < ApplicationController
   def index
-    @games = Game.all
-    render json: GameSerializer.new(@games)
+    games = Game.all
+    render json: games
   end
 
   def create
